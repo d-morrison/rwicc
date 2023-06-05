f9d4db3bfdcec3c8ef4fba344c8c32742267c75b
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2023-06-05 16:23:11 -0700

    I added S to the grouping arguments.

--- R/simulate_interval_censoring.R
+++ R/simulate_interval_censoring.R
@@ -196,7 +196,7 @@ simulate_interval_censoring <- function(
   sim_obs_data =
     sim_participant_data %>%
     dplyr::reframe(
-      .by = c(ID, E, R, `exit date`, `years from study start to seroconversion`),
+      .by = c(ID, E, R, S, `exit date`, `years from study start to seroconversion`),
       `Obs ID` = 1:length(post_seroconversion_obs_dates),
       "O" = R + post_seroconversion_obs_dates
     ) %>%
@@ -238,7 +238,7 @@ simulate_interval_censoring <- function(
   # remove variables not needed for analysis:
   {
     sim_participant_data %<>% dplyr::select(ID, E, L, R, S)
-    sim_obs_data %<>% dplyr::select(ID, E, O, Y, `MAA status`, `Obs ID`)
+    sim_obs_data %<>% dplyr::select(ID, E, O, Y, S, `MAA status`, `Obs ID`)
     }
 
   return(list(
