c2cd303a76758aef6bb09ce8ea458a32152a97aa
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2024-02-01 00:08:32 -0700

    added function

---
+++ R/graph_simulated_survival_curves.R
@@ -0,0 +1,27 @@
+graph_simulated_survival_curves = function()
+{
+
+  ggplot() +
+    theme_classic() +
+    theme(
+      text = element_text(size = 15),
+      legend.position = "bottom"
+    ) +
+    ggplot2::geom_function(
+      fun = seroconversion_survival_function,
+      args = list(intercept = 0, slope = .5), aes(col = "0+0.5t")) +
+    ggplot2::geom_function(
+      fun = seroconversion_survival_function, args = list(intercept = 0, slope = 1), aes(col = "0+1t")) +
+    ggplot2::geom_function(fun = seroconversion_survival_function, args = list(intercept = 0, slope = 2), aes(col = "0+2t")) +
+    ggplot2::geom_function(fun = seroconversion_survival_function, args = list(intercept = 1, slope = 0), aes(col = "1+ 0t")) +
+    ggplot2::geom_function(fun = seroconversion_survival_function, args = list(intercept = 1, slope = .5), aes( col = "1+0.5t")) +
+    ggplot2::geom_function(alpha = .5, fun = seroconversion_survival_function, args = list(intercept = 10,slope = 0), aes(col = "10+ 0t")) +
+    ggplot2::geom_function(linetype = 2, alpha = .5, fun = seroconversion_survival_function, args = list(intercept = 10,slope = .5), aes( col = "10+0.5t")) +
+    xlim(0, 2) +
+    xlab("Time since study start (years)") +
+    ylim(0, 1) +
+    ylab("p(S≥s)") +
+    # ggplot2::scale_y_log10() +
+    ggplot2::labs(col = "")
+
+ }
