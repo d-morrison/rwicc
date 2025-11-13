ef339a88d7acae0ee76e6021f1acec1805e7497b
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2025-11-13 10:38:20 -0800

    fix lint

--- tests/testthat/test-graph_omega.R
+++ tests/testthat/test-graph_omega.R
@@ -3,6 +3,7 @@ test_that("results are consistent", {
     fs::path_package("rwicc", "extdata/example_model.rds") |>
     readr::read_rds()
   omega_est_EM <- example_model$Omega
-  omega_est_EM |> graph_omega() |>
+  omega_est_EM |>
+    graph_omega() |>
     vdiffr::expect_doppelganger(title = "example-model")
 })
