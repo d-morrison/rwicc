a62c1aa8ed7ef3365e2d553b9cd0ae8d0f757012
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2025-11-12 18:31:27 -0800

    fix test

--- tests/testthat/test-graph_omega.R
+++ tests/testthat/test-graph_omega.R
@@ -1,7 +1,8 @@
 test_that("results are consistent", {
   example_model <-
-    fs::path_package("rwicc", "extdata/example_model.rds")
-  omega_est_EM <- EM_algorithm_outputs$Omega
+    fs::path_package("rwicc", "extdata/example_model.rds") |>
+    readr::read_rds()
+  omega_est_EM <- example_model$Omega
   omega_est_EM |> graph_omega() |>
     vdiffr::expect_doppelganger(title = "example-model")
 })
